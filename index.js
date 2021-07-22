eb798349d3cdde0be32f2ee30e08bc0f458d00ae
graceedev <gedavies6@gmail.com>
2021-07-22 12:38:45 +0000

    Add text that is revealed by clicking on how to play text

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Show text when 'how to play' is clicked */
+function clickHowTo() {
+    var x = document.getElementById("how-to-text");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
