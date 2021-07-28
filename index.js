e17b3df3c4b344d11955cce99914c4b3bca9687b
graceedev <gedavies6@gmail.com>
2021-07-28 21:00:50 +0000

    Add word bank

--- index.js
+++ index.js
@@ -32,3 +32,16 @@ let i = 0;
 for (i = 0; i < cards.length; i++) {
 cards[i].addEventListener('click', cardClicked);
 }
+
+/* Generate random words from word bank*/
+
+var wordBank = {'Croeso': Welcome, 'Cwrw': Beer, 'Da': Good, 'Bore': Morning, 
+'Prynhawn': Afternoon, 'Nos': Night, 'Cymraeg': Welsh, 'Saesneg': English,
+'Diolch': Thanks, 'Enw': Name, 'Moron': Carrot, 'Bara': Bread, 'Afal': Apple,
+'Selsig': Sausage, 'Hwyl Fawr': Goodbye, 'Te': Tea, 'Gwin': Wine, 'Dwr': Water,
+'Coffi': Coffee, 'Sosban': Saucepan, 'Yfed': Drink, 'Bwyta': Eat, 'Ci': Dog,
+'Cath': Cat, 'Wy': Egg, 'Meicrodon': Microwave, 'Siaced': Jacket, 'Esgidiau': Shoes,
+'Sgert': Skirt, 'Iawn': Very, 'Ofnadwy': Awful, 'Nofio': Swimming, 'Ydw': Yes, 'Nac ydw': No,
+'Newydd': New, 'Eto': Again, 'Brecwast': Breakfast, 'Dydd': Day, 'Crys': Shirt, 'Llaeth': Milk,
+'Mwynhau': Enjoy, 'Hoffi': Like, 'Un': One, 'Dau': Two, 'Tre': Three, 'Pedwar': Four, 'Pump': Five,
+'Llyfrgell': Library };
