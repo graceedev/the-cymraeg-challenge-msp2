a61972f6dea3a683d3d894d60e5899fe590e7346
graceedev <gedavies6@gmail.com>
2021-07-29 17:07:00 +0000

    Seperate six random keys and values into english and welsh

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function clickHowTo() {
   }
 }
 
-/* Start game - reveal cards*/
+/* Start game - reveal cards, remove play button*/
 function changeCards() {
 let cards = Array.from(document.getElementsByClassName('card'));
 
@@ -18,69 +18,80 @@ for (let i = 0; i < cards.length; i++) {
 
 var x = document.getElementById("btn-play");
   x.style.display = "none";   
-  var wordBank = {
-    Welcome: 'Croeso',
-    Beer: 'Cwrw',
-    Good: 'Da',
-    Morning: 'Bore',
-    Afternoon: 'Prynhawn',
-    Night: 'Nos',
-    Welsh: 'Cymraeg',
-    English: 'Saesneg',
-    Thanks: 'Diolch',
-    Name: 'Enw',
-    Carrot: 'Moron',
-    Bread: 'Bara',
-    Apple: 'Afal',
-    Sausage: 'Selsig',
-    Goodbye: 'Hwyl Fawr',
-    Tea: 'Te',
-    Wine: 'Gwin',
-    Water: 'Dwr',
-    Coffee: 'Coffi',
-    Saucepan: 'Sosban',
-    Drink: 'Yfed',
-    Eat: 'Bwyta',
-    Dog: 'Ci',
-    Cat: 'Cath',
-    Egg: 'Wy',
-    Microwave: 'Meicrodon',
-    Jacket: 'Siaced',
-    Shoes: 'Esgidiau',
-    Skirt: 'Sgert',
-    Very: 'Iawn',
-    Awful: 'Ofnadwy',
-    Swimming: 'Nofio',
-    Yes: 'Ydw',
-    No: 'Nac ydw',
-    New: 'Newydd',
-    Again: 'Eto',
-    Breakfast: 'Brecwast',
-    Day: 'Dydd',
-    Shirt: 'Crys',
-    Milk: 'Llaneth',
-    Enjoy: 'Mwynhau',
-    Like: 'Hoffi',
-    One: 'Un',
-    Two: 'Dau',
-    Three: 'Tre',
-    Four: 'Pedwar',
-    Five: 'Pump',
-    Library: 'Llyfrgell',
-  };
-  var keys = Object.keys(wordBank);
-  var values = Object.values(wordBank);
-  length = keys.length;
-  
-  var i,
-    result = [];
-  for (i = 0; i < 6; i++) {
-    result.push(wordBank[keys[Math.floor(Math.random() * length)]]);
-    result.push(wordBank[values[Math.floor(Math.random() * length)]]);
-  }
-  console.log(result);
-  }
 
+/* Generate random set of six word pairs*/
+  var wordBank = [
+    {'Welcome': 'Croeso'},
+    {'Beer': 'Cwrw'},
+    {'Good': 'Da'},
+    {'Moring': 'Bore'},
+    {'Afternoon': 'Prynhawn'},
+    {'Night': 'Nos'},
+    {'Welsh': 'Cymraeg'},
+    {'English': 'Saesneg'},
+    {'Thanks': 'Diolch'},
+    {'Name': 'Enw'},
+    {'Carrot': 'Moron'},
+    {'Bread': 'Bara'},
+    {'Apple': 'Afal'},
+    {'Sausage': 'Selsig'},
+    {'Goodbye': 'Hwyl Fawr'},
+    {'Tea': 'Te'},
+    {'Wine': 'Gwin'},
+    {'Water': 'Dwr'},
+    {'Coffee': 'Coffi'},
+    {'Saucepan': 'Sosban'},
+    {'Drink': 'Yfed'},
+    {'Eat': 'Bwyta'},
+    {'Dog': 'Ci'},
+    {'Cat': 'Cath'},
+    {'Egg': 'Wy'},
+    {'Microwave': 'Meicrodon'},
+    {'Jacket': 'Siaced'},
+    {'Shoes': 'Esgidiau'},
+    {'Skirt': 'Sgert'},
+    {'Very': 'Iawn'},
+    {'Awful': 'Ofnadwy'},
+    {'Swimming': 'Nofio'},
+    {'Yes': 'Ydw'},
+    {'No': 'Nac ydw'},
+    {'New': 'Newydd'},
+    {'Again': 'Eto'},
+    {'Breakfast': 'Brecwast'},
+    {'Day': 'Dydd'},
+    {'Shirt': 'Crys'},
+    {'Milk': 'Llaneth'},
+    {'Enjoy': 'Mwynhau'},
+    {'Like': 'Hoffi'},
+    {'One': 'Un'},
+    {'Two': 'Dau'},
+    {'Three': 'Tre'},
+    {'Four': 'Pedwar'},
+    {'Five': 'Pump'},
+    {'Library': 'Llyfrgell'},
+  ];
+
+  const shuffle = (wordBank) 
+    let oldElement;
+    for (let i = wordBank.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      oldElement = wordBank[i];
+      wordBank[i] = wordBank[rand];
+      wordBank[rand] = oldElement;
+      var wordBankShuffle = wordBank.slice(0, 6);
+    }
+/*Seperate the English and Welsh translations*/
+    var iterator = wordBankShuffle.values();
+
+    for (const value of iterator) {
+      console.log(value);
+      var welsh = Object.values(value);
+      console.log(welsh);
+      var english = Object.keys(value);
+      console.log(english);
+    };
+ 
+}
 
 /* Click cards to reveal words */
 function cardClicked(event) {
@@ -93,8 +104,6 @@ let i = 0;
 for (i = 0; i < cards.length; i++) {
 cards[i].addEventListener('click', cardClicked);
 }
-
-
 
 
 
