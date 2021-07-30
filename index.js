a442b7fd70d648dc29d15599aac071a78a305b9c
graceedev <gedavies6@gmail.com>
2021-07-30 11:09:17 +0000

    Change layout of wordBank

--- index.js
+++ index.js
@@ -1,67 +1,107 @@
 /* global wordbank variable */
 var wordBank = [
-  {'Welcome': 'Croeso'},
-  {'Beer': 'Cwrw'},
-  {'Good': 'Da'},
-  {'Morning': 'Bore'},
-  {'Afternoon': 'Prynhawn'},
-  {'Night': 'Nos'},
-  {'Welsh': 'Cymraeg'},
-  {'English': 'Saesneg'},
-  {'Thanks': 'Diolch'},
-  {'Name': 'Enw'},
-  {'Carrot': 'Moron'},
-  {'Bread': 'Bara'},
-  {'Apple': 'Afal'},
-  {'Sausage': 'Selsig'},
-  {'Goodbye': 'Hwyl Fawr'},
-  {'Tea': 'Te'},
-  {'Wine': 'Gwin'},
-  {'Water': 'Dwr'},
-  {'Coffee': 'Coffi'},
-  {'Saucepan': 'Sosban'},
-  {'Drink': 'Yfed'},
-  {'Eat': 'Bwyta'},
-  {'Dog': 'Ci'},
-  {'Cat': 'Cath'},
-  {'Egg': 'Wy'},
-  {'Microwave': 'Meicrodon'},
-  {'Jacket': 'Siaced'},
-  {'Shoes': 'Esgidiau'},
-  {'Skirt': 'Sgert'},
-  {'Very': 'Iawn'},
-  {'Awful': 'Ofnadwy'},
-  {'Swimming': 'Nofio'},
-  {'Yes': 'Ydw'},
-  {'No': 'Nac ydw'},
-  {'New': 'Newydd'},
-  {'Again': 'Eto'},
-  {'Breakfast': 'Brecwast'},
-  {'Day': 'Dydd'},
-  {'Shirt': 'Crys'},
-  {'Milk': 'Llaneth'},
-  {'Enjoy': 'Mwynhau'},
-  {'Like': 'Hoffi'},
-  {'One': 'Un'},
-  {'Two': 'Dau'},
-  {'Three': 'Tre'},
-  {'Four': 'Pedwar'},
-  {'Five': 'Pump'},
-  {'Library': 'Llyfrgell'},
+  {'english': 'Welcome',
+  'welsh': 'Croeso'},
+  {'english': 'Beer',
+  'welsh': 'Cwrw'},
+  {'english': 'Good',
+  'welsh': 'Da'},
+  {'english':'Morning',
+  'welsh': 'Bore'},
+  {'english': 'Afternoon',
+   'welsh': 'Prynhawn'},
+  {'english': 'Night',
+  'welsh': 'Nos'},
+  {'english': 'Welsh',
+  'welsh': 'Cymraeg'},
+  {'english': 'English',
+  'welsh': 'Saesneg'},
+  {'english': 'Thanks',
+  'welsh': 'Diolch'},
+  {'english': 'Name',
+  'welsh': 'Enw'},
+  {'english': 'Carrot',
+  'welsh': 'Moron'},
+  {'english': 'Bread',
+  'welsh': 'Bara'},
+  {'english': 'Apple',
+  'welsh': 'Afal'},
+  {'english': 'Sausage',
+  'welsh': 'Selsig'},
+  {'english': 'Goodbye',
+  'welsh': 'Hwyl Fawr'},
+  {'english': 'Tea',
+  'welsh': 'Te'},
+  {'english': 'Wine',
+  'welsh': 'Gwin'},
+  {'english': 'Water',
+  'welsh': 'Dwr'},
+  {'english': 'Coffee',
+  'welsh': 'Coffi'},
+  {'english': 'Saucepan',
+  'welsh': 'Sosban'},
+  {'english': 'Drink',
+  'welsh': 'Yfed'},
+  {'english':'Eat',
+  'welsh': 'Bwyta'},
+  {'english':'Dog',
+  'welsh': 'Ci'},
+  {'english': 'Cat',
+  'welsh': 'Cath'},
+  {'english':'Egg',
+  'welsh': 'Wy'},
+  {'english':'Microwave',
+  'welsh': 'Meicrodon'},
+  {'english': 'Jacket',
+  'welsh': 'Siaced'},
+  {'english': 'Shoes',
+  'welsh': 'Esgidiau'},
+  {'english':'Skirt',
+  'welsh': 'Sgert'},
+  {'english':'Very',
+  'welsh': 'Iawn'},
+  {'english':'Awful',
+  'welsh': 'Ofnadwy'},
+  {'english': 'Swimming',
+  'welsh': 'Nofio'},
+  {'english': 'Yes',
+  'welsh': 'Ydw'},
+  {'english':'No',
+  'welsh': 'Nac ydw'},
+  {'english': 'New',
+  'welsh': 'Newydd'},
+  {'english': 'Again',
+  'welsh': 'Eto'},
+  {'english': 'Breakfast',
+  'welsh': 'Brecwast'},
+  {'english': 'Day',
+  'welsh': 'Dydd'},
+  {'english': 'Shirt',
+  'welsh': 'Crys'},
+  {'english': 'Milk',
+  'welsh': 'Llaneth'},
+  {'english': 'Enjoy',
+  'welsh': 'Mwynhau'},
+  {'english': 'Like',
+  'welsh': 'Hoffi'},
+  {'english': 'One',
+  'welsh': 'Un'},
+  {'english': 'Two',
+  'welsh': 'Dau'},
+  {'english': 'Three',
+  'welsh': 'Tre'},
+  {'english': 'Four',
+  'welsh': 'Pedwar'},
+  {'english': 'Five',
+  'welsh': 'Pump'},
+  {'english': 'Library',
+  'welsh': 'Llyfrgell'},
 ];
+
+const cardContainer = [];
 /* global welsh and english variables */ 
 var welsh = [];
 var english = [];
-
-/* Show text when 'how to play' is clicked */
-function clickHowTo() {
-  var x = document.getElementById("how-to-text");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
 
 /* Start game - reveal cards, remove play button*/
 function changeCards() {
@@ -84,28 +124,5 @@ var x = document.getElementById("btn-play");
       wordBank[rand] = oldElement;
       var wordBankShuffle = wordBank.slice(0, 6);
     }
-/*Seperate the English and Welsh translations*/
-    var iterator = wordBankShuffle.values();
-
-    for (const value of iterator) {
-      console.log(value);
-      welsh.push(Object.values(value));
-      english.push(Object.keys(value));
-    };
- console.log(welsh);
- console.log(english);
-}
-
-/* Click cards to reveal words */
-function cardClicked(event) {
-this.style.background = "var(--dk-green)";
-this.firstChild.classList.remove("hidden");
-}
-
-let cards = document.getElementsByClassName('card');
-let i = 0;
-for (i = 0; i < cards.length; i++) {
-cards[i].addEventListener('click', cardClicked);
-}
-
-
+    console.log(wordBankShuffle)
+  }
